21224b01554e18f1b667ceae98c25ddf4b782fd3
Parker <parker.preyer1@gmail.com>
2018-06-25 15:29:43 -0400

    adds create question and answer and deployed to heroku

--- doggo/app/controllers/answers_controller.rb
+++ doggo/app/controllers/answers_controller.rb
@@ -2,4 +2,25 @@ class AnswersController < ApplicationController
   def show
     @answer = Answer.find(params[:id])
   end
+
+  def create
+    @answer = Answer.new(answer_params)
+    @question = Question.find(params[:question_id])
+    if @answer.save
+      respond_to do |format|
+        format.json { render :show, status: 201 }
+      end
+    else
+      respond_to do |format|
+        format.json { render @answer.errors, status: 400 }
+      end
+    end
+  end
+
+  private
+
+  def answer_params
+    params.permit(:value, :question_id)
+  end
+
 end
--- doggo/app/controllers/questions_controller.rb
+++ doggo/app/controllers/questions_controller.rb
@@ -7,5 +7,23 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
-  
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save        
+      respond_to do |format|
+        format.json { render :show, status: 201 }
+      end
+    else
+      respond_to do |format|
+        format.json { render @question.errors, status: 400 }
+      end
+    end
+  end
+
+  private
+
+  def question_params
+    params.permit(:content)
+  end
 end
